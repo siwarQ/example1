a47609246a27110d02c8aec17b0deed9ad49f17a
siwarQ <socoro92@hotmail.com>
2014-12-03 00:39:09 +0200

    HAHA

--- geryes/src/geryes/test2.java
+++ geryes/src/geryes/test2.java
@@ -5,7 +5,12 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("fgdfgd");
+		
 
+	}
+	
+	public static void sayHello(){
+		System.out.println("VERONICA!!");		
 	}
 
 }
